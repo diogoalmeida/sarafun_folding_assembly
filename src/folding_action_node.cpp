cd1483b023865052cbcd28209772ad3125fdbd8e
Diogo Almeida <diogoa@kth.se>
2017-11-01 09:56:51 +0100

    Add a wrench manager class for subscribing to n wrench topics and providing wrench measurements at the gripping points

--- src/folding_action_node.cpp
+++ src/folding_action_node.cpp
@@ -1,5 +1,6 @@
 #include <ros/ros.h>
 #include <utils/kdl_manager.hpp>
+#include <utils/wrench_manager.hpp>
 #include <folding_assembly_controller/ects.hpp>
 #include <folding_assembly_controller/kalman_filter.hpp>
 #include <folding_assembly_controller/pose_controller.hpp>
@@ -7,10 +8,11 @@
 
 int main(int argc, char ** argv)
 {
-  // folding_algorithms::ECTSController ects_controller;
   folding_algorithms::KalmanEstimator kalman_filter;
   folding_algorithms::FoldingPoseController pose_controller;
   folding_algorithms::AdaptiveController adaptive_velocity_controller;
   folding_utils::KDLManager kdl_manager("blah");
+  folding_utils::WrenchManager wrench_manager();
+  folding_algorithms::ECTSController ects_controller("rod", "surface", std::shared_ptr<folding_utils::KDLManager>(&kdl_manager));
   return 0;
 }
