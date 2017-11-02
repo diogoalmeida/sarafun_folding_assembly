0339fb2421049b14f265050b871b3cb0b6a1afe2
Diogo Almeida <diogoa@kth.se>
2017-11-02 18:09:10 +0100

    Implement folding controller from template class. Add folding goal

---
+++ include/folding_assembly_controller/folding_action_node.hpp
@@ -0,0 +1,47 @@
+#ifndef __FOLDING_ACTION_NODE__
+#define __FOLDING_ACTION_NODE__
+
+#include <ros/ros.h>
+#include <stdexcept>
+#include <generic_control_toolbox/kdl_manager.hpp>
+#include <generic_control_toolbox/wrench_manager.hpp>
+#include <generic_control_toolbox/controller_template.hpp>
+#include <folding_assembly_controller/ects.hpp>
+#include <folding_assembly_controller/kalman_filter.hpp>
+#include <folding_assembly_controller/pose_controller.hpp>
+#include <folding_assembly_controller/FoldingControllerAction.h>
+#include <folding_assembly_controller/adaptive_velocity_controller.hpp>
+
+using namespace folding_assembly_controller;
+class FoldingController : public generic_control_toolbox::ControllerTemplate<FoldingControllerAction,
+                                                                                FoldingControllerGoal,
+                                                                                FoldingControllerFeedback,
+                                                                                FoldingControllerResult>
+{
+public:
+  FoldingController(const std::string &action_name);
+  virtual ~FoldingController();
+
+
+private:
+  bool parseGoal(boost::shared_ptr<const FoldingControllerGoal> goal);
+  void resetController();
+  sensor_msgs::JointState controlAlgorithm(const sensor_msgs::JointState &current_state, const ros::Duration &dt);
+
+  /**
+    Get the required parameters for initializing the folding controller
+    elements.
+
+    @return False if something goes wrong, true otherwise.
+  **/
+  bool init();
+
+  ros::NodeHandle nh_;
+  folding_algorithms::KalmanEstimator kalman_filter_;
+  folding_algorithms::FoldingPoseController pose_controller_;
+  folding_algorithms::AdaptiveController adaptive_velocity_controller_;
+  std::shared_ptr<generic_control_toolbox::KDLManager> kdl_manager_;
+  generic_control_toolbox::WrenchManager wrench_manager_;
+  std::shared_ptr<folding_algorithms::ECTSController> ects_controller_;
+};
+#endif
--- src/folding_action_node.cpp
+++ src/folding_action_node.cpp
@@ -1,42 +1,60 @@
-#include <ros/ros.h>
-#include <generic_control_toolbox/kdl_manager.hpp>
-#include <generic_control_toolbox/wrench_manager.hpp>
-#include <generic_control_toolbox/controller_template.hpp>
-#include <folding_assembly_controller/ects.hpp>
-#include <folding_assembly_controller/kalman_filter.hpp>
-#include <folding_assembly_controller/pose_controller.hpp>
-#include <folding_assembly_controller/FoldingControllerAction.h>
-#include <folding_assembly_controller/adaptive_velocity_controller.hpp>
+#include <folding_assembly_controller/folding_action_node.hpp>
 
 using namespace folding_assembly_controller;
-class FoldingController : public generic_control_toolbox::ControllerTemplate<FoldingControllerAction,
-                                                                                FoldingControllerFeedback,
-                                                                                FoldingControllerResult>
+
+FoldingController::FoldingController(const std::string &action_name) : ControllerTemplate<FoldingControllerAction,
+                                                                        FoldingControllerGoal,
+                                                                        FoldingControllerFeedback,
+                                                                        FoldingControllerResult>(action_name)
 {
-public:
-  FoldingController(const std::string &action_name) : ControllerTemplate<FoldingControllerAction,
-                                                       FoldingControllerFeedback,
-                                                       FoldingControllerResult>(action_name) {}
-  virtual ~FoldingController() {}
+  nh_ = ros::NodeHandle("~");
 
-  sensor_msgs::JointState updateControl(const sensor_msgs::JointState &current_state, const ros::Duration &dt)
+  if (!init())
   {
-    sensor_msgs::JointState ret;
-
-    return ret;
+    throw std::logic_error("Missing parameters for the folding controller");
   }
-private:
-  void goalCB() {}
-  void preemptCB() {}
+}
 
-  folding_algorithms::KalmanEstimator kalman_filter_;
-  folding_algorithms::FoldingPoseController pose_controller_;
-  folding_algorithms::AdaptiveController adaptive_velocity_controller_;
-  std::shared_ptr<generic_control_toolbox::KDLManager> kdl_manager_;
-  generic_control_toolbox::WrenchManager wrench_manager_;
-  std::shared_ptr<folding_algorithms::ECTSController> ects_controller_;
-};
+FoldingController::~FoldingController() {}
 
+bool FoldingController::init()
+{
+  std::string base_link;
+  if (!nh_.getParam("kinematic_chain_base_link", base_link))
+  {
+    ROS_ERROR("Missing kinematic_chain_base_link parameter");
+    return false;
+  }
+
+  kdl_manager_.reset(new generic_control_toolbox::KDLManager(base_link));
+}
+
+sensor_msgs::JointState FoldingController::controlAlgorithm(const sensor_msgs::JointState &current_state, const ros::Duration &dt)
+{
+  sensor_msgs::JointState ret;
+
+  return ret;
+}
+
+bool FoldingController::parseGoal(boost::shared_ptr<const FoldingControllerGoal> goal)
+{
+  try
+  {
+    ects_controller_.reset(new folding_algorithms::ECTSController(goal->rod_eef, goal->surface_eef, kdl_manager_));
+  }
+  catch(std::logic_error &e)
+  {
+    ROS_ERROR("Exception when initializing the ECTS controller: %s.", e.what());
+    return false;
+  }
+
+  return true;
+}
+
+void FoldingController::resetController()
+{
+
+}
 
 int main(int argc, char ** argv)
 {
@@ -44,7 +62,7 @@ int main(int argc, char ** argv)
   folding_algorithms::FoldingPoseController pose_controller;
   folding_algorithms::AdaptiveController adaptive_velocity_controller;
   generic_control_toolbox::KDLManager kdl_manager("blah");
-  generic_control_toolbox::WrenchManager wrench_manager();
+  generic_control_toolbox::WrenchManager wrench_manager;
   folding_algorithms::ECTSController ects_controller("rod", "surface", std::shared_ptr<generic_control_toolbox::KDLManager>(&kdl_manager));
   FoldingController controller("action_name");
   return 0;
