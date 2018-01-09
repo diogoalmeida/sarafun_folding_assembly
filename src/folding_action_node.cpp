b9434a53f8f70fab480e60f144cb3ae1fcb154cd
Diogo Almeida <diogoa@kth.se>
2018-01-09 08:25:43 +0100

    Add option to disable the folding controller

--- src/folding_action_node.cpp
+++ src/folding_action_node.cpp
@@ -1,14 +1,29 @@
 #include <folding_assembly_controller/folding_controller.hpp>
 #include <sensor_msgs/JointState.h>
+#include <std_msgs/Bool.h>
 
-sensor_msgs::JointState state;
-bool got_first;
+sensor_msgs::JointState state_;
+bool got_first_, is_disabled_;
 
 void jointStatesCb(const sensor_msgs::JointState::ConstPtr &msg)
 {
   ROS_INFO_ONCE("Joint state received!");
-  state = *msg;
-  got_first = true;
+  state_ = *msg;
+  got_first_ = true;
+}
+
+void disableCb(const std_msgs::Bool::ConstPtr &msg)
+{
+  if (msg->data == true)
+  {
+    ROS_WARN("Folding controller disabled");
+    is_disabled_ = true;
+  }
+  else
+  {
+    ROS_WARN("Folding controller enabled");
+    is_disabled_ = false;
+  }
 }
 
 int main(int argc, char ** argv)
@@ -24,18 +39,20 @@ int main(int argc, char ** argv)
 
   folding_assembly_controller::FoldingController controller(action_name);
 
+  is_disabled_ = false;
+  got_first_ = false;
   ros::Subscriber joint_state_sub = n.subscribe("/joint_states", 1000, jointStatesCb);
+  ros::Subscriber disable_sub = n.subscribe("/folding/disable", 1000, disableCb);
   ros::Publisher state_pub = n.advertise<sensor_msgs::JointState>("/yumi/joint_command", 1000);
   ros::Rate loop_rate(100);
   ros::Time prev_time = ros::Time::now();
   sensor_msgs::JointState command;
-  got_first = false;
 
   while(ros::ok())
   {
-    if (got_first)
+    if (got_first_ && !is_disabled_)
     {
-      command = controller.updateControl(state, ros::Time::now() - prev_time);
+      command = controller.updateControl(state_, ros::Time::now() - prev_time);
       state_pub.publish(command);
     }
     else
